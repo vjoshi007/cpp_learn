53f888f1cc98b0334be67e49905c461a640660d7
Vivek Joshi <joshiviv007@gmail.com>
2025-05-11 19:10:11 +0300

    New line without flushing the buffer

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 int main()
 {
     int x {6};
-    int y {6};
+    int y {7};
     int z;
     
     z = x*y;
-    std::cout << "Value of z as calculated is: " << z << std::endl;
+    std::cout << "Value of z as calculated is: " << z << '\n';
     std::cout << "HELLO WORLD";
     return 0;
 }
