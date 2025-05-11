8b0e201f5c9b8468261249ae96755270360820e6
Vivek Joshi <joshiviv007@gmail.com>
2025-05-11 17:57:25 +0300

    printing with end of line included

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 int main()
 {
-    std::cout << "hello, world";
+    int x {6};
+    int y {6};
+    int z;
+    
+    z = x*y;
+    std::cout << "Value of z as calculated is: " << z << std::endl;
+    std::cout << "HELLO WORLD";
     return 0;
 }
